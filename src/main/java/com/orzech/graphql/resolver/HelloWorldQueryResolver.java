fbd5e99bd8218f7c84fb550baddaf02df05b3bec
Orzech <orzechowski87@gmail.com>
2021-11-02 22:11:30 +0100

    Hello World - Parametrized method

--- src/main/java/com/orzech/graphql/resolver/HelloWorldQueryResolver.java
+++ src/main/java/com/orzech/graphql/resolver/HelloWorldQueryResolver.java
@@ -3,10 +3,16 @@ package com.orzech.graphql.resolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class HelloWorldQueryResolver implements GraphQLQueryResolver {
 
     public String helloWorld() {
         return "Sample return String from hello world query";
+    }
+
+    public String parametrizedQuery(String firstArg, String secondArg) {
+        return String.format("Parametrized method %s %s", firstArg, Optional.ofNullable(secondArg).orElse(""));
     }
 }
